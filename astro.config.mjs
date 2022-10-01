43c4cc3b1dde0c6e59c3d441ca2ec1a6460d44fb
Kai <webmaster@impactfactory.ch>
2022-10-01 13:42:49 +0200

    integrated sitemap package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
-
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [// Enable Preact to support Preact JSX components.
   preact(), // Enable React for the Algolia search component.
-  react(), image()],
-  site: `http://astro.build`
+  react(), image(), sitemap(
+    {  
+      i18n: {
+        defaultLocale: 'de', 
+        locales: {
+          de: 'de-CH',      
+          en: 'en-US',
+        },
+      },
+    }
+  )],
+  site: `http://astro.build` //hier muss die finale adresse rein https://kunde.ch
 });
