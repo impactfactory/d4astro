b6f8aad3fff292c89073d4a8268eb23a439e7d24
Kai <webmaster@impactfactory.ch>
2022-10-01 16:35:14 +0200

    copy pasted d4dess theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
-import image from "@astrojs/image";
+//import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [// Enable Preact to support Preact JSX components.
   preact(), // Enable React for the Algolia search component.
-  react(), image(), sitemap(
+  react(), 
+  //image(), 
+  sitemap(
     {  
       i18n: {
         defaultLocale: 'de', 
@@ -17,7 +19,7 @@ export default defineConfig({
           en: 'en-US',
         },
       },
-    }
+    },
   )],
   site: `http://astro.build` //hier muss die finale adresse rein https://kunde.ch
 });
